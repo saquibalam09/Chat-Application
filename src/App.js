7583e77ad15b1de36c9ec85b101e53543a6ed23e
saquib <saquibm860@gamil.com>
2024-04-10 04:26:47 +0530

    files are being updated

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
     <>
       <ChatEngine 
         height="100vh"
-        projectID=""
-        userName={localStorage.getItem('username')}
-        userSecret={localStorage.getItem('password')}
-        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        projectID="6d678a44-7e2f-4047-8483-a32c9fbb59be"
+        userName="nazmussaquib"
+        userSecret="12345678"
+        
         onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
       />
     </>
